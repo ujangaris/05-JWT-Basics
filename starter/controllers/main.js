2fde7083cb6805fc26ae92c6c5585dba2fe0cb1d
ujangaja <ujangaja@gmail.com>
2021-11-19 03:32:24 +0700

    #Section 8: JWT - Basics | 158. Sign JWT

--- starter/controllers/main.js
+++ starter/controllers/main.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const CustomeAPIError = require('../errors/custom-error');
 const login = async (req, res) => {
   const { username, password } = req.body;
@@ -8,8 +9,18 @@ const login = async (req, res) => {
   if (!username || !password) {
     throw new CustomeAPIError('Please provide email and password', 400);
   }
-  console.log(username, password);
-  res.send('Fake Login/Register/Signup Route');
+
+  // just for demo, normally provide by DB!!!
+  const id = new Date().getDate();
+
+  // try to keep payload small, better experience for user
+  // just for demo, in production use log, complex and unguessable string value!!!
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30D',
+  });
+
+  // console.log(username, password);
+  res.status(200).json({ msg: 'user created', token });
 };
 
 const dashboard = async (req, res) => {
