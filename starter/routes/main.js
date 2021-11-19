25203297fb83509729c33ae8e97fee14a02d4d09
ujangaja <ujangaja@gmail.com>
2021-11-19 16:49:20 +0700

    #Section 8: JWT - Basics | 163. Auth Middleware Setup

--- starter/routes/main.js
+++ starter/routes/main.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 const { login, dashboard } = require('../controllers/main');
 
-router.route('/dashboard').get(dashboard);
-router.route('/login').post(login);
+const authMiddleware = require('../middleware/auth');
 
-module.exports = router;
+router.route('/dashboard').get(authMiddleware, dashboard);
+router.route('/login').post(login);
 
 module.exports = router;
