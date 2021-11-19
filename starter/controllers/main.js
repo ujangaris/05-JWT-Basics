7205dafdb43fd29ce43b08e71799c6a15aeec535
ujangaja <ujangaja@gmail.com>
2021-11-19 17:09:24 +0700

    #Section 8: JWT - Basics | Basics | 164. Auth Middleware Complete

--- starter/controllers/main.js
+++ starter/controllers/main.js
@@ -24,25 +24,14 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  const authHeader = req.headers.authorization;
+  console.log(req.user);
 
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    throw new CustomeAPIError('No token provided', 401);
-  }
+  const luckyNumber = Math.floor(Math.random() * 100);
 
-  const token = authHeader.split(' ')[1];
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    // console.log(decoded);
-    const luckyNumber = Math.floor(Math.random() * 100);
-    res.status(200).json({
-      msg: `Hello, ${decoded.username}`,
-      secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
-    });
-  } catch (error) {
-    throw new CustomeAPIError('Not authorized to access this route', 401);
-  }
+  res.status(200).json({
+    msg: `Hello, ${req.user.username}`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  });
 };
 
 module.exports = {
--- starter/middleware/auth.js
+++ starter/middleware/auth.js
@@ -1,6 +1,23 @@
+const jwt = require('jsonwebtoken');
+const CustomeAPIError = require('../errors/custom-error');
+
 const authenticationMiddleware = async (req, res, next) => {
-  console.log(req.headers.authorization);
-  next();
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new CustomeAPIError('No token provided', 401);
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const { id, username } = decoded;
+    req.user = { id, username };
+    next();
+  } catch (error) {
+    throw new CustomeAPIError('Not authorized to access this route', 401);
+  }
 };
 
 module.exports = authenticationMiddleware;
